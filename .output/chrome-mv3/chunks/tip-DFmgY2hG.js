4b576789b843aea36a1ecc9dc4e6481282ad6014
song-xp <770027833@qq.com>
2026-06-21 13:37:15 +0800

    feat: handle Extension context invalidated with friendly tip

---
+++ .output/chrome-mv3/chunks/tip-DFmgY2hG.js
@@ -0,0 +1,1 @@
+import{A as r,B as s}from"./popup-_0L6lLyz.js";import"./_virtual_wxt-html-plugins-DPbbfBKe.js";const o="";function t(e){s({message:o+e,type:"error"})}const p=r(t,1e3);export{p as sendErrorMessage};
